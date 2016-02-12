14156f1597f3d198d08dc4474f49efa20fac49b7
Kyle Derrick <kderrick1988@gmail.com>
2016-02-12 08:17:01 -0800

    add specs to remove a and e from string with passing code

--- src/test/java/WordPuzzleTest.java
+++ src/test/java/WordPuzzleTest.java
@@ -5,9 +5,15 @@ import java.util.ArrayList;
 
 public class WordPuzzleTest {
   @Test
-  public void vowelRemover_RemoveAllAsFromString() {
+  public void vowelRemover_RemoveAllAsFromString_lowercase() {
     WordPuzzle testWordPuzzle = new WordPuzzle();
     String letterARemoved = "h-t";
     assertEquals(letterARemoved, testWordPuzzle.vowelRemover("hat"));
+  }
+  @Test
+  public void vowelRemover_RemoveAllEsFromString_lowercase() {
+    WordPuzzle testWordPuzzle = new WordPuzzle();
+    String letterARemoved = "h--l";
+    assertEquals(letterARemoved, testWordPuzzle.vowelRemover("heel"));
   }
 }
